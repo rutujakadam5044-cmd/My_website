a7f145e09cca08bec30f607d3a3ac72772b23fa2
rutujakadam5044-cmd <rutujakadam5044@gmail.com>
2026-01-15 11:11:50 +0530

    Add event listeners for buttons and menu toggle

--- Program.js
+++ Program.js
@@ -1,1 +1,28 @@
-
+let helloBtn =
+document.getElementById("helloBtn");
+helloBtn.addEventListener("click", function () {
+  alert ("Hello ! Welcome to my website");
+});
+let submitBtn =
+document.getElementById("submitBtn");
+let nameInput =
+document.getElementById("nameInput");
+submitBtn.addEventListener("click", function () {
+  let name = nameInput.value;
+  if (name === "") {
+    alert("please enter your name");
+  } else {
+    alert("Hello" + name + "! Thanks for visiting 😊");
+  }
+  }) ;
+  let menuBtn =
+  document.getElementById("menuBtn");
+  let menuContent =
+  document.getElementById("menuContent");
+  menuBtn.addEventListener("click", function () {
+    if (menuContent.style.display === "block") {
+      menuContent.style.display = "none";
+    } else {
+    menuContent.style.display= "block";
+  }
+  }) ;
